8ec5388f7c3cf66851cd71f82f7f80c58c166e1c
Gregory Thomason <gregory_thomason@hotmail.com>
2015-04-23 11:29:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 $(document).ready(function() {
 
-//-----------------------------------------------------------------//
-//-------------------------- SITE ACTIONS -------------------------//
-//-----------------------------------------------------------------//
- 	$('#cover').show();
-	
-	
-	// Window function on page load, scroll, and resize events	
- 	//---------------------------------------------------//	
-	$(window).on("resize scroll load",function(e){		
-		// Force #cover to fill whole page
-		var winHeight = $(window).height();
-		$('#cover').css('min-height', winHeight );
-	});
-
-	$('.arrowdown').live('click',function(event){
-		$('#cover').addClass('swipe');
-		scrollRevealSettings();
-		setTimeout( function() {
-			$('body, html').css('overflow-y', 'visible');
-		}, 1000);
-	});
-
  	// Detect direction of scrolling on page	
  	//---------------------------------------------------//
 	function coverControl() {		
